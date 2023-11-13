5ff51adbf773da61dadd8739d041a05968b1918a
namalwamillia <145693220+namalwamillia@users.noreply.github.com>
2023-11-13 11:39:47 +0300

    Add files via upload
    object oriented programming assignment

---
+++ Object orientet programming(OOP)/opp.py
@@ -0,0 +1,33 @@
+
+class User:
+  def __init__(person, name, age,location):
+    person.name = name
+    person.age = age
+    person.location = location
+#create a user classer with properties ie name,age,location
+class User:
+  def __init__(person, name, age):
+    person.name = name
+    person.age = age
+    
+
+p1 = User("Miria", 20)
+
+print(p1.name)
+print(p1.age)
+#create a faction for the user class that prints the users location.
+# print user location using this function
+class User:
+  def __init__(person, location):
+    person.location = location
+    
+p1 = User("kampala")
+print(p1.location)
+  
+  
+
+
+
+
+
+
